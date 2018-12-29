6f2b69a5bd0e3bf07a18d8975f4de2a7de770208
Salahzar Stenvaag <salahzar@gmail.com>
2018-12-29 12:48:44 +0100

    added html

---
+++ hifi/sal/scripts/simplestteleport.js
@@ -0,0 +1,19 @@
+// basta che mettiate l'indirizzo (Navigation, Copy Address to Clipboard) dentro le properties 
+// p.es. hifi://librovivo/1033.29,741.358,-245.998/0,-0.540938,0,0.841063
+// 2018 CC0, written by Salahzar Stenvaag
+(function () {
+    
+    this.clickDownOnEntity = function (entityID, mouseEvent) {
+      try {
+            address = Entities.getEntityProperties(entityID, ["position", "userData"]);
+            print("Teleport verso "+address);
+            location = address;     
+          } catch (e) { 
+            print("Error: "+e);
+          }
+      }
+    }
+
+    this.mousePressOnEntity = this.clickDownOnEntity;
+
+})
