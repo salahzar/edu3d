b0cd6d2b2d3e6ac70b5caf7d33b3f0247d48c00a
Salahzar Stenvaag <salahzar@gmail.com>
2021-08-19 00:39:44 +0200

    Update soundLoopEmitterClick.js

--- hifi/sal/scripts/soundLoopEmitterClick.js
+++ hifi/sal/scripts/soundLoopEmitterClick.js
@@ -1,9 +1,18 @@
 (function () {
 
+    // position Vec3 (Vec3.ZERO)
+    // orientation Quaternion (Quaternion.IDENTITY)
+    // volume Number 1.0
+    // pitch 0.0625 – 16.0   1 no change in pitch min +- 2 octaves
+    // loop boolean false
+    // secondOffset number 0.0
+    // localOnly boolean false
+    // { "soundURL":"http","position": Vec3.ZERO, "volume": 1.0, "pitch": 1.0, "loop": false, "secondOffset": 0.0, "localOnly": false }
     var soundURL = null;//SoundCache.getSound(SOUND_URL);
     var lastSoundURL = null;
     var soundVolume = null;
     var soundData = null;
+    var pitch = null;
     var injector = null;
     var properties = null;
 
@@ -19,18 +28,20 @@
             print("SoundURL " + soundData.soundURL);
             // need to check that all this stuff even exists and throw error if not.s
             soundURL = SoundCache.getSound(soundData.soundURL);
-            receiverName = soundData.receiverName;
+            //receiverName = soundData.receiverName;
             soundVolume = !isNaN(soundData.soundVolume) ? Number(soundData.soundVolume) : 0.0;
             soundLoop = soundData.isLoop;
             soundLocal = soundData.isLocal;
+            pitch = soundData.pitch;
         } catch (e) { }
 
 
         injector = Audio.playSound(soundURL, {
             position: properties.position,
             volume: soundVolume,
-            loop: false,//soundLoop,
-            localOnly: true//;//soundLocal
+            loop: soundLoop,
+            localOnly: soundLocal,
+            pitch: pitch
         });
 
     };
